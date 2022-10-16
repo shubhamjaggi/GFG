a70961a12634dff064d4b334931f4c0857e3c50e
shubhamjaggi <shubham.jaggi@gmail.com>
2022-10-16 15:02:52 +0530

    added solution for 'remove-duplicates-from-an-unsorted-linked-list'

---
+++ Linked List/remove-duplicates-from-an-unsorted-linked-list/Solution.java
@@ -0,0 +1,39 @@
+
+/* The structure of linked list is the following
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d; 
+        next = null;
+    }
+}
+*/
+
+class Solution
+{
+    //Function to remove duplicates from unsorted linked list.
+    public Node removeDuplicates(Node head) 
+    {
+        if(head==null || head.next==null) return head;
+        
+        Set<Integer> nums = new HashSet<>();
+        
+        nums.add(head.data);
+        Node prev = head;
+        Node curr = head.next;
+        
+        while(curr!=null){
+            if(nums.contains(curr.data)){
+                prev.next = curr.next;
+            }else{
+                prev = curr;
+                nums.add(curr.data);
+            }
+            curr = curr.next;
+        }
+        
+        return head;
+    }
+}
